d0ead9d0ec60feea6495391dbcb59933dbf90424
César Navarro Bibiloni <cesnavar@ucm.es>
2024-03-04 12:07:11 +0100

    conflicto 1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //conflicto 1
+            Console.WriteLine("Conflicto1");
         }
     }
 }
